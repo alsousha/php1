25c543ddca80796a9397afd783f9c3f045b5ff27
Alsu <alsousha@mail.ru>
2020-01-20 14:28:48 +0200

    task1.4

---
+++ gb4_2/layout.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header><?php include('blocks/header.php')?></header>
+    <div class="content"><?php include('blocks/content.php')?></div>
+    <footer><?php include('blocks/footer.php')?></footer>    
+</body>
+</html>
